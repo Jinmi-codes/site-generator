2452b83ac4ca1c0fefb44aab7a6fae80040f0481
Jinmi-codes <jinmiolaosoko@gmail.com>
2026-03-31 16:25:10 +0100

    md to html func

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,7 +1,8 @@
 import re
 from enum import Enum
 from functions import markdown_to_blocks, text_to_textnodes
-from htmlnode import text_node_to_html_node, HTMLnode
+from htmlnode import text_node_to_html_node, HTMLnode, ParentNode
+from textnode import TextNode, TextType
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph block"
@@ -52,7 +53,45 @@ def markdown_to_html_node(markdown):
     for block in blocks:
         match block_to_block_type(block):
             case BlockType.PARAGRAPH:
-                block_node = HTMLnode('p',block, text_to_children(block))
+                block = " ".join(block.split("\n"))
+                block_node = ParentNode('p',text_to_children(block))
                 new_blocks.append(block_node)
                 
+            case BlockType.ORDERED_LIST:
+                items = [line.split(". ")[1] for line in block.split("\n")]
+                li_nodes = []
+                for item in items:
+                    li_nodes.append(ParentNode('li', text_to_children(item)))
+                block_node = ParentNode('ol', li_nodes)
+                new_blocks.append(block_node)
                 
+            case BlockType.UNORDERED_LIST:
+                items = [line[2:] for line in block.split("\n")]
+                li_nodes = []
+                for item in items:
+                    li_nodes.append(ParentNode('li', text_to_children(item)))
+                block_node = ParentNode('ul', li_nodes)
+                new_blocks.append(block_node)
+                
+            case BlockType.QUOTE:
+                items = [line[2:] for line in block.split("\n")]
+                block_node = ParentNode('blockquote', text_to_children("\n".join(items)))
+                new_blocks.append(block_node)
+            
+            case BlockType.CODE:
+                content = block[3:-3]
+                block_node = ParentNode('pre', [ParentNode('code', [text_node_to_html_node(TextNode(content, TextType.TEXT)) ])])
+                new_blocks.append(block_node)
+            
+            case BlockType.HEADING:
+                hashes = 0
+                for letter in block:
+                    if letter == '#':
+                        hashes+=1
+                    else:
+                        break
+                content = block[hashes + 1:].strip()
+                block_node = ParentNode(f"h{hashes}", text_to_children(content))
+                new_blocks.append(block_node)
+                        
+    return ParentNode("div", new_blocks)
